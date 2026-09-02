89b4fe0941c98c20b0678e2fe8c3dcaf8d4f7581
Sean Pettas <seanpettas@gmail.com>
2026-09-02 12:53:24 +0200

    Refactored parsing
    Refactored the csv reading into inputParser.cs so we can work on it without merge conflicts coming in the way

---
+++ Bison.CLI/inputParser.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class inputParser
+{
+    public List<string[]> getFileData(string filePath)
+    {
+        List<string[]> rows = new();
+
+        try
+        {
+            string[] lines = File.ReadAllLines("./data/bison_observe_cli_db.csv");
+
+            foreach (string line in lines)
+            {
+                string[] values = line.Split(",");
+                rows.Add(values);
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        return rows;
+    }
+}
