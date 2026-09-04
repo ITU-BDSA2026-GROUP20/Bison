103c8edb1bc4e76e01d98e08d541d426b16fecd1
Mikkel Thomsen <mikth@itu.dk>
2026-09-04 11:51:28 +0200

    Refactored with Oskar's CLI Argument Main() indifference Co-authored-by: William Noes <wino@itu.dk>

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,13 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using Bison.CLI;
+﻿using System.CommandLine;
 using Bison.CLI.models;
+using Bison.CLI;
 
 class Program
 {
-    static void Main(string[] args)
+    static int Main(string[] args)
+    {
+        inputParser parser = new inputParser();
+
+        var readCommand = new Command("read", "Print recorded observations");
+        readCommand.SetAction(parseResult =>
+        {
+            List<Reading> lines = parser.getFileData("./Bison.CLI/data/bison_observe_cli_db.csv");
+            UserInterface.printOutput(lines);
+        });
+
+        var observationArgument = new Argument<string>("observation")
+        {
+            Description = "The observation to record"
+        };
+
+        var observeCommand = new Command("observe", "Record a new observation");
+        observeCommand.Arguments.Add(observationArgument);
+        observeCommand.SetAction(parseResult =>
+        {
+            handleObservation(parseResult.GetValue(observationArgument)!);
+            UserInterface.printObservationRecorded();
+        });
+
+        var root = new RootCommand("Bison observation tracker")
+        {
+            Subcommands = { readCommand, observeCommand }
+        };
+
+        return root.Parse(args).Invoke();
+    }
+
+    
+    
+    static void handleObservation(string observation)
+    {
+        string author = Environment.UserName;
+        string timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+        string filePath = "./Bison.CLI/data/bison_observe_cli_db.csv";
+
+        try
+        {
+            using (StreamWriter sw = File.AppendText(filePath))
+            {
+                sw.WriteLine($"{author},\"{observation}\",{timestamp}");
+            }
+        }
+        catch (Exception e)
+        {
+            UserInterface.printExeptionError(e);
+        }
+
+    }
+}
+ /*
+ static void Main(string[] args)
     {
         inputParser parser = new inputParser();
 
@@ -32,24 +84,4 @@ class Program
                 break;
         }
     }
-    
-    static void handleObservation(string observation)
-    {
-        string author = Environment.UserName;
-        string timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
-        string filePath = "./Bison.CLI/data/bison_observe_cli_db.csv";
-
-        try
-        {
-            using (StreamWriter sw = File.AppendText(filePath))
-            {
-                sw.WriteLine($"{author},\"{observation}\",{timestamp}");
-            }
-        }
-        catch (Exception e)
-        {
-            UserInterface.printExeptionError(e);
-        }
-
-    }
-}
+ */
