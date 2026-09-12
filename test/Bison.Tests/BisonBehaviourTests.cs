169b4a2b5da66bc3dbaa7911621071d425393ba3
Peachy006 <seanpettas@gmail.com>
2026-09-12 19:35:26 +0200

    First test and refactor
    Added first test and refactored DiscussionCommand slightly to handle exception on Guid parsing, this was also to give a proper output for the test.

---
+++ test/Bison.Tests/BisonBehaviourTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+namespace Bison.Tests;
+
+public class BisonBehaviourTests
+{
+    [Fact]
+    public async Task PrintsCorrectErrorOnInvalidCommentId()
+    {
+        string expected = "Unrecognized id format. Id needs to be a Guid";
+
+        var psi = new ProcessStartInfo
+        {
+            FileName = "dotnet",
+            Arguments = $"run --project \"{GetCliProjectPath()}\" -- discussion invalid-id",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+        };
+
+        using var process = Process.Start(psi)!;
+        string output = await process.StandardOutput.ReadToEndAsync();
+        await process.WaitForExitAsync();
+
+        Assert.Contains(expected, output);
+    }
+
+    private static string GetCliProjectPath([CallerFilePath] string here = "")
+        => Path.GetFullPath(Path.Combine(Path.GetDirectoryName(here)!, "..", "..", "src", "Bison.CLI", "Bison.CLI.csproj"));
+}
