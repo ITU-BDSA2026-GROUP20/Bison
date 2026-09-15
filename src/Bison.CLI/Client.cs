d3143d06043b5ae7ff655cc20a952175792614a6
Peachy006 <seanpettas@gmail.com>
2026-09-15 22:23:32 +0200

    Basic client setup, WORK IN PROGRESS
    Just had to switch computers with Gabriel so this is a temp commit
    
    Co-Authored-By: Gabriel Efferbach  <259585294+efferbach92-cmyk@users.noreply.github.com>

---
+++ src/Bison.CLI/Client.cs
@@ -0,0 +1,30 @@
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Bison.CLI.models;
+
+namespace Bison.CLI;
+
+public static class Client
+{
+    private const string BaseUrl = "http://localhost:5251";
+
+    public static HttpClient Instance { get; } = BuildClient();
+
+    private static HttpClient BuildClient()
+    {
+        var client = new HttpClient { BaseAddress = new Uri(BaseUrl) };
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        return client;
+    }
+
+    public static async Task<T?> GetAsync<T>(string endpoint)
+    {
+        var response = await Instance.GetAsync(endpoint);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadFromJsonAsync<T>();
+    }
+
+
+}
